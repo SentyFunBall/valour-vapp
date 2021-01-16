3ce3d35b310566141068d346faf8a32e5f0a660c
follow-the-programmer <arthurargams@gmail.com>
2021-01-15 21:48:51 -0300

    added token class

--- spaguettiCode.cs
+++ spaguettiCode.cs
@@ -14,4 +14,23 @@ namespace vapp
         public string TT_RPAREN   = "RPAREN";
         public string digits      = "1234567890";
     }
+    
+    public class Token{
+        private string type;
+        private dynamic value;
+        public Token(string type, dynamic value){
+            this.type = type;
+            this.value = value;
+        }
+        public Token(string type){
+            this.type = type;
+        }
+
+        public override string ToString(){
+            if (this.value){
+                return $"{this.type}:{this.value}";
+            }
+            return $"{this.type}";
+        }
+    }
 }
