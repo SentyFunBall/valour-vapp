a41ddb1d0c0d704195876b7dd0ffc430aa53f6a5
follow-the-programmer <arthurargams@gmail.com>
2021-01-15 21:47:07 -0300

    Create spaguettiCode.cs
    beacuse everyone knows that spaguetti code is the best one

---
+++ spaguettiCode.cs
@@ -0,0 +1,17 @@
+using System;
+using Sytem.Collections.Generic;
+
+namespace vapp
+{
+    public class Ttandkw{
+        public string TT_INT	  = "INT";
+        public string TT_FLOAT    = "FLOAT";
+        public string TT_PLUS     = "PLUS";
+        public string TT_MINUS    = "MINUS";
+        public string TT_MUL      = "MUL";
+        public string TT_DIV      = "DIV";
+        public string TT_LPAREN   = "LPAREN";
+        public string TT_RPAREN   = "RPAREN";
+        public string digits      = "1234567890";
+    }
+}
