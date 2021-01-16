9fc8953775ae2f49266b4971ac4d8ef11e3db818
follow-the-programmer <arthurargams@gmail.com>
2021-01-16 13:28:34 -0300

    shell 1.0.2 now easier to read the code

---
+++ .vapp/_Constants.py
@@ -0,0 +1,49 @@
+DIGITS = '0123456789'
+LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+LETTERS_DIGITS = LETTERS + DIGITS
+
+TT_INT = 'INT'
+TT_FLOAT = 'FLOAT'
+TT_STRING = 'STRING'
+TT_IDENTIFIER = 'IDENTIFIER'
+TT_KEYWORD = 'KEYWORD'
+TT_PLUS = 'PLUS'
+TT_MINUS = 'MINUS'
+TT_MUL = 'MUL'
+TT_DIV = 'DIV'
+TT_POW = 'POW'
+TT_EQ = 'EQ'
+TT_LPAREN = 'LPAREN'
+TT_RPAREN = 'RPAREN'
+TT_LSQUARE = 'LSQUARE'
+TT_RSQUARE = 'RSQUARE'
+TT_EE = 'EE'
+TT_NE = 'NE'
+TT_LT = 'LT'
+TT_GT = 'GT'
+TT_LTE = 'LTE'
+TT_GTE = 'GTE'
+TT_COMMA = 'COMMA'
+TT_ARROW = 'ARROW'
+TT_NEWLINE = 'NEWLINE'
+TT_EOF = 'EOF'
+
+KEYWORDS = [
+    'var',
+    'and',
+    'or',
+    'NOT',
+    'if',
+    'elif',
+    'else',
+    'for',
+    'until',
+    'step',
+    'while',
+    'define',
+    'then',
+    'end',
+    'return',
+    'continue',
+    'break',
+]
