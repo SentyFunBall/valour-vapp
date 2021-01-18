3e60c7de6dcfda7764d6c75273e287c7521183b2
Senty Funball <haydenbugatti@gmail.com>
2021-01-17 21:12:23 -0800

    rebuilt btn.js

--- nautilus/src/btn.js
+++ nautilus/src/btn.js
@@ -2,6 +2,18 @@ const {ipcMain, dialog} = require('electron')
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const os = require('os')
+const {ipcRenderer} = require('electron')
+
+const loadB = document.getElementById('loadBtn')
+const newB = document.getElementById('newBtn')
+
+loadB.addEventListener('click', (event) => {
+  ipcRenderer.send('open-file-dialog')
+})
+
+newB.addEventListener('click', (event) => {
+    ipcRenderer.send('open-error-dialog')
+})
 
 ipcMain.on('open-file-dialog', (event) => {
     dialog.showOpenDialog({
