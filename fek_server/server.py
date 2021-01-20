ea5821a5fa43de14fbfdc5db3208e57713d8b1a1
follow-the-programmer <arthurargams@gmail.com>
2021-01-20 16:00:39 -0300

    better authentication
    now is  prime * prime if they arent the same

--- fek_server/server.py
+++ fek_server/server.py
@@ -7,16 +7,43 @@ api = Api(app)
 # TODO: add rsa validation or something asymmetric, symmetric validation is cursed
 
 
-def validate_token(token):
-    if token == 1:
+def is_prime(n):
+    if n == 1:
         return False
     i = 2
-    while i * i <= token:
-        if token % i == 0:
+    while i * i <= n:
+        if n % i == 0:
             return False
         i += 1
 
     return True
+
+
+def try_division(tok):
+    n = 2
+    while tok % n != 0:
+        n += 1
+        if n >= tok:
+            return tok
+    return n
+
+
+def authenticate(token):
+    div1 = try_division(token)
+
+    if not is_prime(div1):
+        return False
+    token /= div1
+    if not is_prime(token):
+        return False
+
+    if div1 == token or token % 2 == 0:
+        return False
+    return True
+
+
+def validate_token(token):
+    return authenticate(token)
 
 
 class LastMessage(Resource):
--- fek_server/test.py
+++ fek_server/test.py
@@ -2,5 +2,5 @@ import requests
 
 Base = 'http://127.0.0.1:5000/'  # use this for the test server, later we will use the
 
-response = requests.get(Base + '/api/1/get/test')
+response = requests.get(Base + '/api/15/get/test')
 print(response.json())
