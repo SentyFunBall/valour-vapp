ac63234c695d1bec9d49435aca5f9969645fb918
droplt-the-programmer <72446297+follow-the-programmer@users.noreply.github.com>
2021-01-22 17:45:44 -0300

    api bois

--- wrapper/Valour.py
+++ wrapper/Valour.py
@@ -1,10 +1,56 @@
 import requests
+import authenticate
 
+##########################################
+#
+# TODO: add a way to work only with tokens
+#
+##########################################
 
-class ValourBot:
-
-    def __init__(self, token):
+class Api:
+    def __init__(self, token, system):
         self.token = token
-        self.authenticate()
+        self.isAuth = self.authenticate(token)
+        self.system = system
+        self.ValourApi = f"Valour.gg/{system}/api"
+        self.lastApiCall = {}
 
-    def authenticate
+    def authenticate(self, token):
+        return authenticate.authenticate(token)
+
+    def getfromApi(self, system):
+        if self.isAuth:
+            response = requests.get(self.ValourApi)
+            response = response.json()
+            if not response == self.lastApiCall:
+                if response["lastMessage"] != self.lastApiCall["lastMessage"]:
+                    self.lastApiCall = response
+                    self.onMessageSent()
+                elif response["newUser"] != self.lastApiCall["newUser"]:
+                    self.lastApiCall = response
+                    self.onUserJoin()
+                elif response["createdNew"] != self.lastApiCall["createdNew"]:
+                    self.lastApiCall = response
+                    self.onNew(self.lastApiCall["createdNew"])            
+                else:
+                    self.lastApiCall = response
+        else:
+            return "this is an invalid token!!!"
+
+    def onMessageSent():
+        pass # changed by the user
+
+    def onUserJoin():
+        pass # changed by the user
+    
+    def onNew(what):
+        pass # changed by the user
+        # passed args:
+        # moon, planet, None
+
+    def sendMessage(self, messageContent, pathToPlanet):
+        mapped_request = {"sentOn": (self.system + pathToPlanet), "content": messageContent}
+        requests.post(self.ValourApi, data=mapped_request)
+
+if __name__ == "__main__":
+    print("lets goo")
