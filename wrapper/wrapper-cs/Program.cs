821420427a80f22b496aeda6eba4a1e9548ce822
follow-the-programmer <arthurargams@gmail.com>
2021-01-26 17:16:30 -0300

    things are nicely organized and started c# wrapper

---
+++ wrapper/wrapper-cs/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+namespace wrapper_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sysName = "sysname";
+            string url = $"Valour.gg/{sysName}/api";
+            WebRequest get = WebRequest.Create(url);
+            Stream stream = get.GetResponse().GetResponseStream()
+        }
+    }
+}
