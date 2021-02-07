9dba7093c02a71f37929911ecd6cec088d3af536
follow-the-programmer <arthurargams@gmail.com>
2021-02-06 23:48:45 -0300

    i n s t a l l e r

--- installers/installer.bat
+++ installers/installer.bat
@@ -8,15 +8,23 @@ echo using Valour Aplication ide version %release%!!
 echo this installer works for repair and for installing nautilus / wrappers
 pause
 color 07
+cls
 set /p folderpath=where did you unzip the file, if you did on the desktop area will be: C:\Users\(user)\desktop\Nautilus
 IF EXIST %root% () ELSE (
     mkdir %root%
 )
 move %folderpath%\wrapper %root%
 move %folderpath%\Nautilus\* %root%
+move %folderpath%\run.bat %root%
 cd %root%\Nautilus 
 unzip -o %root%\Nautilus\blockly.zip -d %root%\Nautilus\blockly 
 del /f %root%\Nautilus\blockly.zip
+echo installing dependencies, ignore things that will pop up in the screen
+pause
+cls
+npm install
+cls
+shortcut /a:c /f:%root%\run.bat /t:%userprofile%\desktop
 echo done!
 echo check %root% hehehe
 pause
