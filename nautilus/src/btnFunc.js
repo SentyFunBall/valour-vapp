0cd7c57257ee38d34e38e5e2d93d2c0180eb430c
follow-the-programmer <arthurargams@gmail.com>
2021-02-11 17:51:23 -0300

    console log bad

--- nautilus/src/btnFunc.js
+++ nautilus/src/btnFunc.js
@@ -24,16 +24,13 @@ function run(){
     // tries the code, just dont do recursion pls
     try{
         eval(Blockly.JavaScript.workspaceToCode(workspace));
-        console.log("no errors, nice.")
     } catch (e) {
         console.log(e);
-        console.log("just errors, nothing too much to worry about :)")
     }
 }
 
 
 function save(){
-    console.log("Opening save dialog")
     dialog.showSaveDialog({
         title: 'Save as',
         defaultPath: path.join(__dirname, '../workspace/'),
@@ -46,14 +43,12 @@ function save(){
         properties: []
     }).then(file => {
         // if file was cancelled by the user, but why?
-        console.log(file.canceled);
         //the workspace to code (js)
         if (!file.canceled) {
-            console.log(file.filePath.toString());
             fs.writeFile(file.filePath.toString(),
             Blockly.JavaScript.workspaceToCode(workspace), function (err) {
                 if (err) throw err;
-                console.log('Saved!');
+                // if saved
             });
             // this is ultra big pog brain moment, the workspace is going to dom, that goes to text(xml)
             // and that is the format we need
@@ -62,27 +57,23 @@ function save(){
                           xml_text, function (err) { if (err) throw err});
           }
     }).catch(err => {
-        console.log(err)
-        dialog.showErrorBox("There was an error saving: ",err)
+        // if error
     });
 }
 
 function loadfiles(){
-    console.log("Opening open dialog")
     dialog.showOpenDialog({properties: ['openFile'] }).then(function (response) {
         if (!response.canceled) {
         var xml = fs.readFileSync(response.filePaths[0]).toString();
-        console.log("" + xml);
         if (xml.startsWith("<xml") && xml.endsWith("</xml>")){            
-            var yes = Blockly.Xml.appendDomToWorkspace(Blockly.Xml.textToDom(xml), workspace);
-            console.log("oh yes, loaded");
+            var yes = Blockly.Xml.appendDomToWorkspace(Blockly.Xml.textToDom(xml), workspace)
         } else {
-            console.log("file couldn't be loaded ):")
+            // if file couldn't be loaded
         }
 
 
         } else {
-        console.log("no file selected");
+        // if no file were selected
         }
     });
 }
@@ -96,7 +87,7 @@ function createVar(){
     })
     .then((r) => {
         if(r === null) {
-            console.log('user cancelled');
+            // if user cancel
         } else {
             workspace.createVariable(r);
         }
