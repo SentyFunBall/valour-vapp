636086b65d2ad32016817d7e8083f8fd0b9cbe36
Senty Funball <haydenbugatti@gmail.com>
2021-02-13 22:16:38 -0800

    New Valour blocks.

--- nautilus/src/blocks.js
+++ nautilus/src/blocks.js
@@ -11,37 +11,180 @@ Blockly.Blocks['token_auth'] = {
   this.setHelpUrl("");
     }
   };
-  Blockly.Blocks['send_message'] = {
+
+//Valour blocks
+Blockly.Blocks['send_message'] = {
   init: function() {
     this.appendValueInput("content")
-      .setCheck("String")
-      .appendField("Message content:");
+        .setCheck("String")
+        .appendField("Message content:");
     this.appendValueInput("author")
-      .setCheck("null")
-      .appendField("Token:");
+        .setCheck(null)
+        .appendField("Token:");
     this.appendValueInput("path")
-      .setCheck("String")
-      .appendField("Message path:");
+        .setCheck("String")
+        .appendField("Message path:");
     this.setInputsInline(false);
     this.setPreviousStatement(true, null);
-    this.setColour(230);
-    this.setTooltip("Send the message.");
-    this.setHelpUrl("");
-  } 
+    this.setNextStatement(true, null);
+    this.setColour(195);
+ this.setTooltip("Send the message.");
+ this.setHelpUrl("");
+  }
 };
 
 Blockly.Blocks['message_get'] = {
- init: function() {
-  this.appendValueInput("get")
-    .setCheck(null)
-    .appendField("Message received:");
-   this.appendStatementInput("no")
-    .setCheck(null)
-    .appendField("do");
-   this.setInputsInline(false);
-   this.setColour(230);
-   this.setTooltip("Start of a code block, executes when a message is caught.");
-   this.setHelpUrl("");
+  init: function() {
+    this.appendValueInput("get")
+        .setCheck(null)
+        .appendField("Message received:");
+    this.appendStatementInput("no")
+        .setCheck(null)
+        .appendField("do");
+    this.setInputsInline(false);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(195);
+ this.setTooltip("Start of a code block, executes when a message is caught.");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['role_add'] = {
+  init: function() {
+    this.appendValueInput("name")
+        .setCheck("String")
+        .appendField("Role to add:");
+    this.appendValueInput("member")
+        .setCheck(null)
+        .appendField("Member to put role on:");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(210);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['role_remove'] = {
+  init: function() {
+    this.appendValueInput("name")
+        .setCheck("String")
+        .appendField("Role to remove:");
+    this.appendValueInput("member")
+        .setCheck(null)
+        .appendField("Member to remove role from:");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(210);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['kick'] = {
+  init: function() {
+    this.appendValueInput("member")
+        .setCheck(null)
+        .appendField("Member to kick:");
+    this.appendValueInput("reason")
+        .setCheck(null)
+        .appendField("Reason:");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(210);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['ban'] = {
+  init: function() {
+    this.appendValueInput("member")
+        .setCheck(null)
+        .appendField("Member to ban:");
+    this.appendValueInput("reason")
+        .setCheck(null)
+        .appendField("Reason:");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(210);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['get_value'] = {
+  init: function() {
+    this.appendValueInput("value")
+        .setCheck(null)
+        .appendField("Value to get:");
+    this.appendValueInput("from")
+        .setCheck(null)
+        .appendField("Get value from:");
+    this.setInputsInline(false);
+    this.setOutput(true, null);
+    this.setColour(210);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['message_author'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Author");
+    this.setOutput(true, null);
+    this.setColour(120);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['message_info'] = {
+  init: function() {
+    this.appendValueInput("info")
+        .setCheck(null)
+        .appendField("Message part:");
+    this.setInputsInline(true);
+    this.setOutput(true, null);
+    this.setColour(160);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['info_planet'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Planet");
+    this.setOutput(true, null);
+    this.setColour(120);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['info_channel'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Channel");
+    this.setOutput(true, null);
+    this.setColour(120);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['message_id'] = {
+  init: function() {
+    this.appendValueInput("id")
+        .setCheck(null)
+        .appendField("Message ID:");
+    this.setOutput(true, null);
+    this.setColour(160);
+ this.setTooltip("");
+ this.setHelpUrl("");
   }
 };
 
@@ -131,22 +274,104 @@ Blockly.Blocks['eval'] = {
     var code = 'var token = ' + value_name + ';';
     return code
   }
-  Blockly.JavaScript['send_message'] = function(block) {
-    var value_content = Blockly.JavaScript.valueToCode(block, 'content', Blockly.JavaScript.ORDER_ATOMIC);
-    var value_author = Blockly.JavaScript.valueToCode(block, 'author', Blockly.JavaScript.ORDER_ATOMIC);
-    var value_path = Blockly.JavaScript.valueToCode(block, 'path', Blockly.JavaScript.ORDER_ATOMIC);
-    // TODO: Assemble JavaScript into code variable.
-    var code = '...;\n';
-    return code;
-  };
 
-  Blockly.JavaScript['message_get'] = function(block) {
-    var value_get = Blockly.JavaScript.valueToCode(block, 'get', Blockly.JavaScript.ORDER_ATOMIC);
-    var statements_no = Blockly.JavaScript.statementToCode(block, 'no');
-    // TODO: Assemble JavaScript into code variable.
-    var code = '//nothing here'
-    return code;
-  };
+//Valour code gens
+
+Blockly.JavaScript['send_message'] = function(block) {
+  var value_content = Blockly.JavaScript.valueToCode(block, 'content', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_author = Blockly.JavaScript.valueToCode(block, 'author', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_path = Blockly.JavaScript.valueToCode(block, 'path', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['message_get'] = function(block) {
+  var value_get = Blockly.JavaScript.valueToCode(block, 'get', Blockly.JavaScript.ORDER_ATOMIC);
+  var statements_no = Blockly.JavaScript.statementToCode(block, 'no');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['role_add'] = function(block) {
+  var value_name = Blockly.JavaScript.valueToCode(block, 'name', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_member = Blockly.JavaScript.valueToCode(block, 'member', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['role_remove'] = function(block) {
+  var value_name = Blockly.JavaScript.valueToCode(block, 'name', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_member = Blockly.JavaScript.valueToCode(block, 'member', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['kick'] = function(block) {
+  var value_member = Blockly.JavaScript.valueToCode(block, 'member', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_reason = Blockly.JavaScript.valueToCode(block, 'reason', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['ban'] = function(block) {
+  var value_member = Blockly.JavaScript.valueToCode(block, 'member', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_reason = Blockly.JavaScript.valueToCode(block, 'reason', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['get_value'] = function(block) {
+  var value_value = Blockly.JavaScript.valueToCode(block, 'value', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_from = Blockly.JavaScript.valueToCode(block, 'from', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['message_author'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['message_info'] = function(block) {
+  var value_info = Blockly.JavaScript.valueToCode(block, 'info', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['info_planet'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['info_channel'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['message_id'] = function(block) {
+  var value_id = Blockly.JavaScript.valueToCode(block, 'id', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
   Blockly.JavaScript['console_print'] = function(block) {
     var value_to_print = Blockly.JavaScript.valueToCode(block, 'to_print', Blockly.JavaScript.ORDER_ATOMIC);
     // TODO: Assemble JavaScript into code variable.
