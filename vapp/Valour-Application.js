639ff100d22373680a8cf4d1372b485d22cbe5b3
follow-the-programmer <arthurargams@gmail.com>
2021-03-02 20:58:36 -0300

    pressure

---
+++ vapp/Valour-Application.js
@@ -0,0 +1,33 @@
+class ValourApp{
+    constructor(bot_token){
+        this.bot_token = bot_token
+    }
+
+    on_message_sent(func){
+        func();
+    }
+}
+class message{
+    constructor(){
+        this.author = "";
+        this.content = "";
+    }
+    getfrom(server) {
+        //do api call with a dict of {message, author} please
+        this.author = 1/*author*/
+        this.content = 1/* content*/
+
+    }
+
+    sendTo(server, message){
+        const message = {
+            planet_Id: null,
+            content: null,
+            date: Date.UTC(),
+            message: message
+            /* idk im on too much pressure */
+        }
+
+        /* api call to send message */
+    }
+}
--- vapp/htmlParser.py
+++
@@ -1,7 +0,0 @@
-from markdown import markdown
-file = input("file name: ")
-with open(file + ".md", 'r') as f:
-    text = f.read()
-
-with open(file + ".html", 'w') as f:
-    f.write(markdown(text))
