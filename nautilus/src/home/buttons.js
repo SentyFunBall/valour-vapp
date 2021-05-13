6078db7488774e2ad574eef91d6e55edb160e946
Senty Funball <haydenbugatti@gmail.com>
2021-05-13 11:04:12 -0700

    fixed home screen buttons again lol

---
+++ nautilus/src/home/buttons.js
@@ -0,0 +1,45 @@
+let shell = require('electron').shell
+
+var codex = document.getElementById("codex");
+var git = document.getElementById("git");
+var wiki = document.getElementById("wiki");
+var docs = document.getElementById("docs");
+var tuts = document.getElementById("tuts");
+var dics = document.getElementById("disc");
+
+codex.addEventListener("click", function(event) {
+    if (event.target.tagName === 'A' && event.target.href.startsWith('http')) {
+        event.preventDefault();
+        shell.openExternal(event.target.href);
+    }
+});
+git.addEventListener("click", function(event) {
+    if (event.target.tagName === 'A' && event.target.href.startsWith('http')) {
+        event.preventDefault();
+        shell.openExternal(event.target.href);
+    }
+});
+wiki.addEventListener("click", function(event) {
+    if (event.target.tagName === 'A' && event.target.href.startsWith('http')) {
+        event.preventDefault();
+        shell.openExternal(event.target.href);
+    }
+});
+docs.addEventListener("click", function(event) {
+    if (event.target.tagName === 'A' && event.target.href.startsWith('http')) {
+        event.preventDefault();
+        shell.openExternal(event.target.href);
+    }
+});
+tuts.addEventListener("click", function(event) {
+    if (event.target.tagName === 'A' && event.target.href.startsWith('http')) {
+        event.preventDefault();
+        shell.openExternal(event.target.href);
+    }
+});
+dics.addEventListener("click", function(event) {
+    if (event.target.tagName === 'A' && event.target.href.startsWith('http')) {
+        event.preventDefault();
+        shell.openExternal(event.target.href);
+    }
+});
